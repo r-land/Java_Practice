eb6ee9e24c981038962b1acb0d3b3d822634195c
조아름 <labofsamplea@gmail.com>
2023-03-20 21:02:44 +0900

    명품자바 chap 2-1

--- MasterpieceJava/chap2_1.java
+++ MasterpieceJava/chap2_1.java
@@ -1,14 +1,12 @@
-
 package MasterpieceJava;  
 import java.util.Scanner;
 public class chap2_1 {
     public static void main(String arge[]){
  Scanner sc =new Scanner(System.in);
- System.out.print("원화를 입력하세요 단위 원)>>>");
+ System.out.print("원화를 입력하세요. (단위 원)>>>");
  int won = sc.nextInt();
  System.out.println(won+"원은 $"+ won/1100.0 +"입니다.");
  sc.close();
-    }
-    
+    }    
 }
 
