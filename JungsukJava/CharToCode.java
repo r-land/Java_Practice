9f734d7c2ffcb83813c51799dd0a979ae91c27b8
조아름 <labofsamplea@gmail.com>
2023-03-24 22:53:27 +0900

    0324 java_practice

---
+++ JungsukJava/CharToCode.java
@@ -0,0 +1,15 @@
+package JungsukJava;
+
+public class CharToCode {
+    public static void main (String args[]) {
+        char ch = 'A';
+        int code = (int)ch;
+
+        System.out.printf("#c=%d(%#X)%n", ch, code, code);
+     
+        
+
+        char hch = '가';
+        System.out.printf("%c=%d(%#X)%n", hch, (int)hch, (int)hch);
+    }
+}
