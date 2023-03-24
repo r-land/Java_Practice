103354d1a1c9bcef133f47b5ebd4bb18143955ef
조아름 <labofsamplea@gmail.com>
2023-03-24 18:07:35 +0900

    0324 java_practice

--- MasterpieceJava/chap2_2.java
+++ MasterpieceJava/chap2_2.java
@@ -5,7 +5,7 @@ public class chap2_2 {
     public static void main(String args[]){
         Scanner sc = new Scanner(System.in);
         System.out.println("2자리수 정수 입력(10~99 >>");
-        int i =Sc.nextInt();
+        int i =sc.nextInt();
         if(i/10 == i%10)
         System.out.println("Yes!10의 자리와 1의 자리가 같습니다.");
         sc.close();
