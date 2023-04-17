8df7b7bff6baa5460270e6ac901e5a9f4abf423b
조아름 <labofsamplea@gmail.com>
2023-04-17 23:56:53 +0900

    0417 java_practice

---
+++ MasterpieceJava/ch5/ColorPoint.java
@@ -0,0 +1,31 @@
+package MasterpieceJava.ch5;
+
+class Point {
+	private int x, y;
+	public Point(int x, int y) {this.x = x; this.y = y;}
+	public int getX() {return x;}
+	public int getY() {return y;}
+	protected void move(int x, int y) {this.x = x; this.y = y;}
+}
+//Point 클래스 사전제공
+//Point 클래스 상속 받은 ColorPoint 클래스  생성자와 메소드 작성
+public class ColorPoint extends Point{
+	private String color;
+	public ColorPoint(int x, int y, String color) {
+		super(x, y);
+		this.color = color;
+	}
+	public void setXY(int x, int y) {move(x,y);}
+	public void setColor(String color) {this.color = color;}
+	public String toString() {
+		return color + "색의 " + "(" + getX() + "," + getY() + ")의 점"; 
+	}
+    //실행결과도출 사전제공 생성자 메소드 유추
+	public static void main(String[] args) {
+		ColorPoint cp = new ColorPoint(5, 5, "YELLOW");
+		cp.setXY(10, 20);
+		cp.setColor("RED");
+		String str = cp.toString();
+		System.out.println(str + "입니다.");
+	}
+}
