3b1a9627cde300d23032fda30039cb5a6b2b1e2f
조아름 <labofsamplea@gmail.com>
2023-05-13 23:32:54 +0900

    0513 java_practice

---
+++ JungsukgichoJava/ch07/Ex7_6.java
@@ -0,0 +1,15 @@
+package JungsukgichoJava.ch07;
+
+
+import static java.lang.System.out;
+import static java.lang.Math.random;
+
+
+public class Ex7_6 {
+        public static void main(String[] args) {
+            out.println(random());
+            
+            out.println("Math.PI :" + Math.PI);
+        }
+    }
+
